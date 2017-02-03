5337981872cfe3abf10f9cc0043174ab58cfa2e9
Samuel Allen <dehuszar@gmail.com>
2017-02-03 10:48:39 -0500

    passing values to function to ensure proper eval

--- app/components/import-sandbox.js
+++ app/components/import-sandbox.js
@@ -3,7 +3,7 @@ import ltHelper from 'ember-truth-helpers/helpers/lt';
 
 export default Ember.Component.extend({
   didInsertElement() {
-    console.log(ltHelper.ltHelper); // returns function
+    console.log(ltHelper.ltHelper([1,2], {})); // returns function
     debugger // accessing ltHelper at breakpoint fails
   }
 });
